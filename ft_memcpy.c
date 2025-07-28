72a0bfaeb2e7b0fb788597290a143a3bda0fad78
Rodrigo Sousa <rosousa-@student.42sp.org.br>
2025-07-28 19:46:35 -0300

    memcpy ok, needs review

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -12,7 +12,7 @@ void *ft_memcpy (void *dest, const void *src, size_t n)
     while(i < n)
     {
         ((char *)dest)[i] = ((char *)src)[i];
-        // *((char *)dest + i) = *((char *)src + i);
+        *((char *)dest + i) = *((char *)src + i);
         i++;
     }
     return(dest);
