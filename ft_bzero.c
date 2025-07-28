df1a8d6610fcb2dea37df32c1592d67f0972f97a
Rodrigo Sousa <rosousa-@student.42sp.org.br>
2025-07-27 23:22:11 -0300

    bzero ok, needs review

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,13 +1,39 @@
 #include <stdlib.h>
+#include <strings.h>
+#include <stdio.h>
 
-void bzero(void *s, size_t n);
+void ft_bzero(void *str, size_t n);
 
-void bzero(void *s, size_t n)
+void ft_bzero(void *str, size_t n)
 {
+    int unsigned i;
+    i = 0;
+
+    while(i < n)
+    {
+        ((char *)str)[i] = '\0';
+        i++;
+    }
 
 }
 
 int main()
 {
+    size_t n;
+    //na original o número de bytes que vamos ter que modificar não pode ser maior que a quantidade de bytes existentes
+    n = 5;
+    char str[] = "Rodrigo";
+    bzero(str, n);
+    // ft_bzero(str, n);
 
+    int i;
+    i = 0;
+    printf("Saída: ");
+    while(i < 7)
+    {
+        printf("%02x", str[i]);
+        i++;
+    }
+
+    return(0);
 }
--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int isalpha(int c);
+int ft_isalpha(int c);
 
-int isalpha(int c)
+int ft_isalpha(int c)
 {
     if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
         return (1);
@@ -14,7 +14,7 @@ int main()
     int c;
     int result;
     c = 91;
-    result = isalpha(c);
+    result = ft_isalpha(c);
     printf("Caracter verificado. Resultado: %d\n", result);
     return(0);
 }
