689d03d11e7eabad7047ec7fb7e32613606b394d
Rodrigo Sousa <rosousa-@student.42sp.org.br>
2025-07-28 19:11:42 -0300

    first commit

---
+++ ft_memcpy.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void *ft_memcpy (void *dest, const void *src, size_t n);
+
+void *ft_memcpy (void *dest, const void *src, size_t n)
+{
+    unsigned int i;
+    i = 0;
+
+    while(i < n)
+    {
+        ((char *)dest)[i] = ((char *)src)[i];
+        // *((char *)dest + i) = *((char *)src + i);
+        i++;
+    }
+    return(dest);
+}
+
+int main()
+{
+    char str1[] = "Rodrigo";
+    char str2[8];
+    printf("Resultado de str2 antes da fnc: %s\n", str2);
+    ft_memcpy(str2, str1, 8);
+    printf("Resultado de str2 depois da fnc: %s\n", str2);
+    return(0);
+}
