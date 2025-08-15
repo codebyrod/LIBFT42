3526c7b9d91bd36ff0bee53166bd4c1677d4f405
Rodrigo Sousa <rosousa-@student.42sp.org.br>
2025-08-15 00:47:29 -0300

    correcting according to the norminnete

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: rosousa- <rosousa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 01:41:52 by rosousa-          #+#    #+#             */
-/*   Updated: 2025/08/14 21:10:49 by rosousa-         ###   ########.fr       */
+/*   Updated: 2025/08/15 00:43:24 by rosousa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int		i;
 	int		number;
@@ -21,15 +21,15 @@ int ft_atoi(const char *str)
 	number = 0;
 	i = 0;
 	signal = 1;
-	while(str[i] >= 9 && str[i] <= 13 || str[i] == 32)
+	while (str[i] >= 9 && str[i] <= 13 || str[i] == 32)
 		i++;
-	if(str[i] == '+' || str[i] == '-')
+	if (str[i] == '+' || str[i] == '-')
 	{
-		if(str[i] == '-')
+		if (str[i] == '-')
 			signal = -1;
 		i++;
 	}
-	while(str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str[i] <= '9')
 	{
 		number = (number * 10) + (str[i] - '0');
 		i++;
--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: rosousa- <rosousa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 21:54:05 by rosousa-          #+#    #+#             */
-/*   Updated: 2025/08/14 23:47:26 by rosousa-         ###   ########.fr       */
+/*   Updated: 2025/08/15 00:44:12 by rosousa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	i = 0;
 	if (size && nmemb > (size_t)-1 / size)
 		return (0);
-		
 	new_len = nmemb * size;
 	ptr = malloc(new_len);
 	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, new_len);
-	// ft_bzero(ptr, nmemb * size);
 	return (ptr);
 }
