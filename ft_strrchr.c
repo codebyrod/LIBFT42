e49773181bc4db7b458b1d77b79ad5088f547bbb
Rodrigo Sousa <rosousa-@student.42sp.org.br>
2025-08-20 20:53:09 -0300

    resolving errors in strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rosousa- <rosousa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 21:59:05 by rosousa-          #+#    #+#             */
-/*   Updated: 2025/08/15 16:57:52 by rosousa-         ###   ########.fr       */
+/*   Updated: 2025/08/20 20:52:25 by rosousa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,14 @@ char	*ft_strrchr(const char *str, int c)
 	char			*temp;
 
 	i = 0;
+	temp = NULL;
 	while (str[i] != '\0')
 	{
-		if (((char *)str)[i] == c)
+		if (((char *)str)[i] == (char)c)
 			temp = ((char *)str + i);
 		i++;
 	}
+	if ((char)c == '\0')
+		return ((char *)str + i);
 	return (temp);
 }
